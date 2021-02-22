6833aef2b7530072d47e66df9225b30c0fe3c137
salma <iddir.salma@gmail.com>
2021-02-22 23:37:51 +0200

    Revert "revert modification on signup and thematiques"
    This reverts commit aad920fb5362c0308708ae4300fcb6545e550293.

--- src/Components/sign_up/SignUp.tsx
+++ src/Components/sign_up/SignUp.tsx
@@ -6,8 +6,7 @@ import Concept from '~/assets/home/question.jpg';
 import ConfirmDialog from '../dialogs/ConfirmDialog';
 import FormSteps from './form-steps';
 import classNames from 'classnames';
-
-import './SignUp.css';
+import "./SignUp.css";
 import { UserReducer, DefaultUser } from './user-reducer';
 
 const jumbotron = {
@@ -23,9 +22,9 @@ const jumbotron = {
 const getClassesForContainer = (step: number) => {
     return classNames({
         'container-transparent': true,
-        'container-transparent-large': step + 1 === 3,
+        'container-transparent-large': step + 1 === 3
     });
-};
+}
 
 const SignUp = (props) => {
     const [step, setStep] = useState(0);
@@ -35,17 +34,17 @@ const SignUp = (props) => {
 
     const handleCancelDialog = () => {
         setShowDialog(false);
-    };
+    }
 
     const handleConfirmDialog = () => {
         setShowDialog(false);
         props.history.push('/'); // Redirect to homepage
-    };
+    }
 
     const handleShowDialog = () => {
         setShowDialog(true);
-        console.log('clicked', showDialog);
-    };
+        console.log("clicked", showDialog);
+    }
 
     return (
         <Jumbotron fluid style={jumbotron} className="d-flex flex-column justify-content-center">
@@ -60,48 +59,36 @@ const SignUp = (props) => {
             />
             <Container className={getClassesForContainer(step)}>
                 <div className="container-header pt-md-5">
-                    <Row style={{ justifyContent: 'space-between', alignItems: 'baseline' }}>
-                        <div>
-                            <h1 className="display-4">{steps[`${step}`].title}</h1>
-                        </div>
-                        <div>
-                            <p style={{ textAlign: 'center' }}>Étape {step + 1}/3</p>
-                        </div>
+                    <Row style={{ justifyContent: "space-between", alignItems: "baseline" }}>
+                        <h1>{steps[`${step}`].title}</h1>
+                        <p style={{ textAlign: "center" }}>Etape {step + 1}/3</p>
                     </Row>
 
-                    <Row style={{ margin: '40px auto', justifyContent: 'space-between' }}>
+                    <Row style={{ margin: "40px auto", justifyContent: "space-between" }}>
                         <div onClick={handleShowDialog} className="link" tabIndex={1}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                className="bi bi-arrow-left-short"
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
-                                />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
                             </svg>
                             Retourner à l'accueil
                         </div>
-                        {step + 1 === 3 ? (
+                        {(step + 1 === 3) ?
                             <div className="link" tabIndex={1}>
                                 <SrollLink
                                     to="submit-button"
                                     spy={true}
                                     smooth={true}
                                     duration={500}
-                                    activeClass="active"
+                                    activeClass='active'
                                 >
                                     Selectionner plus tard
                                 </SrollLink>
                             </div>
-                        ) : null}
+                            : null
+                        }
                     </Row>
 
                     {steps[`${step}`].content}
+
                 </div>
             </Container>
         </Jumbotron>

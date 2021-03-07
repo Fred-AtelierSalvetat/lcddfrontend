a7c6a44a7a34f8e21b1f15185a6c4879e5c8c06f
Salma IDDIR <siddir@sii.fr>
2021-03-08 00:47:35 +0100

    improve css prochain atelier using figma

--- src/Components/home/prochain_atelier/ProchainAtelier.tsx
+++ src/Components/home/prochain_atelier/ProchainAtelier.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import Calendar from './../../../assets/icons/calendar.png';
 
 
@@ -10,55 +10,87 @@ const backgroundTransparency = {
 };
 
 const textContainer = {
-    color: 'white',
-    fontFamily: 'Segoe UI',
+    color: '#FFFFFF',
     textShadow: '1px 1px 2px black',
-};
-
-const title = {
-    fontFamily: 'Segoe UI',
-    fontStyle: 'normal',
-    fontWeight: 300,
-    fontSize: '7vw',
-    position: 'left',
+    size: '30px',
+    marginLeft: '185px',
+    marginBottom: '30px',
+    marginTop: '20px'
 };
 
 const link = {
-    textAlign: 'right' as 'right',
+    width: '82px',
+    height: '24px',
+    style: 'normal',
+    color: '#113F59',
+    size: '21px',
+    weight: '600',
+    innerHeight: '24px',
+
+};
+
+const ateliertitle = {
+    size: '21px',
+    weight: '350',
+    style: 'normal',
+    innerHeight: '23px',
+    width: '111px',
+    height: '23px',
+    color: '#113F59',
+
+};
+
+const date = {
+    size: '16px',
+    weight: '600',
+    style: 'normal',
+    innerHeight: '23px',
+    width: '192px',
+    height: '24px',
+    color: '#113F59',
+
 };
 
 const styledCard = {
     width: '1140px',
     height: '87px',
-    align: 'center',
-    margin: '30px',
     marginLeft: '170px',
-    marginBottom: '100px',
-
+    marginBottom: '50px',
 };
+
+const seeMore = {
+    color: '#20D6C7',
+    position: 'absolute'
+};
+
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 const ProchainAtelierComponent: React.FC<{}> = () => {
     return (
         <div>
-            <div style={backgroundTransparency}>
-                <div>
-                    <h4 style={textContainer}>Prochain Atelier</h4>
+            <Card>
+                <div style={backgroundTransparency}>
+                    <Row>
+                        <h4 style={textContainer}>Prochain Atelier</h4>
+                    </Row>
+                    <div>
+                        <Card style={styledCard}>
+                            <div style={{ margin: '15px' }}>
+                                <Row>
+                                    <Col xs="11">
+                                        <p style={ateliertitle}>Atelier titre</p>
+                                        <p style={date}>20 Mai 2020 : 15:30 - 18:30</p>
+                                    </Col>
+                                    <Col xs="1" >
+                                        <a href="/"><img src={Calendar} alt="calendar" /></a>
+                                        <a style={link} href="/">Reserver</a>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Card>
+                    </div>
                 </div>
-                <div>
-                    <Card style={styledCard}>
-                        <Row>
-                            <Col xs="11">
-                                <p >Atelier titre</p>
-                                <p>20 Mai 2020 : 15:30 - 18:30</p>
-                            </Col>
-                            <Col xs="1" style={{ padding: '15px' }}>
-                                <a href="/"><img src={Calendar} alt="calendar" /></a>
-                                <a href="/">Reserver</a>
-                            </Col></Row>
-                    </Card>
-                </div>
-            </div>
+            </Card>
         </div>
     );
 };
